52d344c56b63e08bb06c86efe9ae0a5024122d82
Rahul16-teach <tiku.rakhasiya@gmail.com>
2026-06-16 14:16:49 +0530

    updating in CSRF policy

--- backend/clickmart_main/settings.py
+++ backend/clickmart_main/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -171,5 +171,5 @@ CORS_ALLOWED_ORIGINS = [
     'https://www.djangoclickmart.shop',
 ]
 
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS","").split(",")
 
